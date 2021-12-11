39b917aa0cd69b326daa90b97382fa6517fb2c5c
RyanDrop <ryangithub7509@gmail.com>
2021-12-11 15:29:40 -0300

    🏗️ build: Ts config

---
+++ src/typings.d.ts
@@ -0,0 +1,24 @@
+declare module "*.html" {
+  const value: string;
+  export default value;
+}
+declare module "*.scss" {
+  const value: { [className: string]: string };
+  export default value;
+}
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
