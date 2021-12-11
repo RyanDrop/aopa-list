4cc1c2f873d7a4f1c4f1d1e6b66a77ef3661bf06
RyanDrop <ryangithub7509@gmail.com>
2021-12-11 15:56:38 -0300

    ✨ feat: PageLog

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,7 @@
-import { Home } from "../pages/home/home.js";
+import { Home } from "../pages/home/home";
+import { Log } from "../pages/log/log";
+
 export const ROUTES = {
   home: Home,
+  log: Log,
 };
