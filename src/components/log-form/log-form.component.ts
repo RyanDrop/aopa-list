378f1537e32de75a9dc17357d86ca09d6b194806
RyanDrop <ryangithub7509@gmail.com>
2021-12-21 11:34:14 -0300

    ✨ feat: Log-form validateData and Logpage hasLogin

--- src/components/log-form/log-form.component.ts
+++ src/components/log-form/log-form.component.ts
@@ -1,10 +1,13 @@
+import { Validators } from "../../classes/validator";
+import { FirebaseServices } from "../../services/firebase.service";
+import { UtilsService } from "../../services/utils.service";
 import logoPath from "../../assets/images/list.png";
 import logFormTemplate from "./log-form.component.html";
 import logFormStyles from "./log-form.component.scss";
-import { FirebaseServices } from "../../services/firebase.service";
 
 export class LogFormComponent extends HTMLElement {
   private firebaseServices: FirebaseServices;
+  private validators: Validators;
   private $tabRegister: HTMLDivElement;
   private $tabLogin: HTMLDivElement;
   private $aLogin: HTMLAnchorElement;
@@ -17,18 +20,20 @@ export class LogFormComponent extends HTMLElement {
   private $registerPassword: HTMLInputElement;
   private $mathPassword: HTMLInputElement;
   private $buttonRegister: HTMLButtonElement;
+  private $loginEmail: HTMLInputElement;
+  private $loginPassword: HTMLInputElement;
+  private $buttonLogin: HTMLButtonElement;
   private customStyle = logFormStyles;
   constructor() {
     super();
+    this.validators = new Validators();
     this.firebaseServices = new FirebaseServices();
   }
 
   connectedCallback(): void {
     this.innerHTML = logFormTemplate;
-
     this.$log = this.querySelector(".log");
     this.$log.src = logoPath;
-
     this.$cardLog = this.querySelector(".card-log");
     this.$aRegister = this.querySelector(".register");
     this.$aLogin = this.querySelector(".login");
@@ -38,21 +43,25 @@ export class LogFormComponent extends HTMLElement {
     this.$aRegister.addEventListener("click", () => {
       this.tabDisplay(this.$tabLogin, this.$tabRegister);
       this.toggleAttributeElements();
+      this.buttonEnable();
     });
     this.$aLogin.addEventListener("click", () => {
       this.tabDisplay(this.$tabRegister, this.$tabLogin);
       this.toggleAttributeElements();
+      this.buttonEnable();
     });
-
-    this.firebaseServices.start();
 
     this.$nameForm = this.querySelector(".input-name");
     this.$occupationForm = this.querySelector(".input-occupation");
     this.$registerEmail = this.querySelector(".register-email");
     this.$registerPassword = this.querySelector(".register-password");
-    this.$mathPassword = this.querySelector(".math-password");
+    this.$mathPassword = this.querySelector(".match-password");
     this.$buttonRegister = this.querySelector(".register-submit");
+    this.$loginEmail = this.querySelector(".login-email");
+    this.$loginPassword = this.querySelector(".login-password");
+    this.$buttonLogin = this.querySelector(".login-submit");
 
+    this.formListenersRegisterValidate();
     this.$buttonRegister.addEventListener("click", () => {
       this.firebaseServices.register(
         this.$registerEmail.value,
@@ -60,6 +69,11 @@ export class LogFormComponent extends HTMLElement {
         this.$nameForm.value,
         this.$occupationForm.value
       );
+    });
+
+    this.formListenersLoginValidate();
+    this.$buttonLogin.addEventListener("click", () => {
+      this.firebaseServices.login(this.$loginEmail.value, this.$loginPassword.value);
     });
   }
 
@@ -74,6 +88,54 @@ export class LogFormComponent extends HTMLElement {
     this.$cardLog.toggleAttribute("login");
   }
 
-  formListeners() {}
+  formListenersRegisterValidate() {
+    this.$nameForm.addEventListener("input", () => {
+      this.validators.isValidName(this.$nameForm.value);
+      UtilsService.elementClassToggleWithCondition(this.$nameForm, "invalid", !this.validators.name);
+      this.buttonEnable();
+    });
+    this.$occupationForm.addEventListener("input", () => {
+      this.validators.isValidOccupation(this.$occupationForm.value);
+      UtilsService.elementClassToggleWithCondition(this.$occupationForm, "invalid", !this.validators.occupation);
+      this.buttonEnable();
+    });
+    this.$registerEmail.addEventListener("input", () => {
+      this.validators.isValidEmail(this.$registerEmail.value);
+      UtilsService.elementClassToggleWithCondition(this.$registerEmail, "invalid", !this.validators.email);
+      this.buttonEnable();
+    });
+    this.$registerPassword.addEventListener("input", () => {
+      this.validators.isValidPassword(this.$registerPassword.value);
+      UtilsService.elementClassToggleWithCondition(this.$registerPassword, "invalid", !this.validators.password);
+      this.buttonEnable();
+    });
+    this.$mathPassword.addEventListener("input", () => {
+      this.validators.passwordIsMatch(this.$registerPassword.value, this.$mathPassword.value);
+      UtilsService.elementClassToggleWithCondition(this.$mathPassword, "invalid", !this.validators.passwordMatch);
+      this.buttonEnable();
+    });
+  }
+
+  formListenersLoginValidate() {
+    this.$loginEmail.addEventListener("input", () => {
+      this.validators.isValidEmail(this.$loginEmail.value);
+      UtilsService.elementClassToggleWithCondition(this.$loginEmail, "invalid", !this.validators.email);
+      this.buttonEnable();
+    });
+    this.$loginPassword.addEventListener("input", () => {
+      this.validators.isValidPassword(this.$loginPassword.value);
+      UtilsService.elementClassToggleWithCondition(this.$loginPassword, "invalid", !this.validators.password);
+      this.buttonEnable();
+    });
+  }
+
+  buttonEnable(): void {
+    const validLogin = !this.validators.loginIsValidAllProperties();
+    const validRegister = !this.validators.registerIsValidAllProperties();
+    this.$buttonRegister.disabled = validRegister;
+    this.$buttonLogin.disabled = validLogin;
+    if (validLogin) this.$buttonLogin.disabled = validLogin;
+    if (validRegister) this.$buttonRegister.disabled = validRegister;
+  }
 }
 customElements.define("aopa-log-form", LogFormComponent);
--- src/pages/log/log.page.ts
+++ src/pages/log/log.page.ts
@@ -1,5 +1,6 @@
-import { WelcomeCardComponent } from "../../components/welcome-card/welcome-card.component";
 import { LogFormComponent } from "../../components/log-form/log-form.component";
+import { WelcomeCardComponent } from "../../components/welcome-card/welcome-card.component";
+import { FirebaseServices } from "../../services/firebase.service";
 import LogPageStyles from "./log.page.scss";
 
 export async function Log(): Promise<HTMLElement> {
@@ -8,15 +9,19 @@ export async function Log(): Promise<HTMLElement> {
 }
 
 export class LogPage extends HTMLElement {
+  private firebaseServices: FirebaseServices;
   private customStyle = LogPageStyles;
   private declarations = [WelcomeCardComponent, LogFormComponent];
   constructor() {
     super();
+    this.firebaseServices = new FirebaseServices();
+    this.firebaseServices.hasLogin$.subscribe((hasUser) => {
+      if (hasUser) window.location.href = "/?#home";
+    });
   }
 
   connectedCallback() {
     const $welcome = document.createElement("aopa-welcome-card");
-
     $welcome.addEventListener("load-log-form", () => {
       const $logForm = document.createElement("aopa-log-form");
       $welcome.remove();
