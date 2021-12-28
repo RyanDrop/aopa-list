c3d89ba10525a698a60d358131f962a8c5ce052f
RyanDrop <ryangithub7509@gmail.com>
2021-12-28 13:41:18 -0300

    ✨ feat: Utils createElementWithText

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -4,4 +4,10 @@ export class UtilsService {
     const methodKeyName = condition ? "add" : "remove";
     $element.classList[methodKeyName](className);
   }
+
+  static createElementWithText(tagName: string, text: string): HTMLElement {
+    const element = document.createElement(tagName);
+    element.textContent = text;
+    return element;
+  }
 }
