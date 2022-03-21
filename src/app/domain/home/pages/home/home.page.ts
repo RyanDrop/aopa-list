5b98e173905b403ec0bd73cb0eeb0005c65b4ce9
RyanDrop <ryangithub7509@gmail.com>
2022-03-21 11:15:49 -0300

    ✨ feat(home domain): home page

---
+++ src/app/domain/home/pages/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '@aopa/services';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'aopa-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+@UntilDestroy()
+export class HomePage implements OnInit {
+  name: string;
+  occupation: string;
+  constructor(private firebase: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.firebase.hasLogin();
+    this.firebase.hasUser$
+      .pipe(take(1), untilDestroyed(this))
+      .subscribe((boolean) => {
+        if (boolean) return;
+      });
+    this.firebase.user$
+      .pipe(take(1), untilDestroyed(this))
+      .subscribe((details) => {
+        this.name = details.user.name;
+        this.occupation = details.user.occupation;
+      });
+  }
+}
