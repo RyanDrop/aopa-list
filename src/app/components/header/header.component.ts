67bc6b0efd264e2c3f203a303aec3e4e35d0498e
RyanDrop <ryangithub7509@gmail.com>
2022-03-21 10:51:27 -0300

    🗑️ revert(components): exclude components form input, edit in place and view input

--- src/app/components/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'aopa-header',
-  template: `<ng-content></ng-content>`,
-  styleUrls: ['./header.component.scss'],
-})
-export class HeaderComponent {}
