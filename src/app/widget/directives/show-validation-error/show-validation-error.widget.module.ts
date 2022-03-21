67b9280aa97f6c4eb90f0b656b5213952e1ae7f4
RyanDrop <ryangithub7509@gmail.com>
2022-03-21 11:10:36 -0300

    ✨ feat(widget): module for show validation error directive

---
+++ src/app/widget/directives/show-validation-error/show-validation-error.widget.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ShowValidationErrorDirective } from './show-validation-error.directive';
+
+@NgModule({
+  declarations: [ShowValidationErrorDirective],
+  exports: [ShowValidationErrorDirective],
+})
+export class ShowValidationErrorDirectiveWidgetModule {}
