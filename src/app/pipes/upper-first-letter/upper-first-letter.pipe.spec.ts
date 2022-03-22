a3af5f9127942c871ed5a303bacd937343611268
RyanDrop <ryangithub7509@gmail.com>
2022-03-22 13:11:47 -0300

    🗑️ revert(pipes): removes all pipes outside the domains

--- src/app/pipes/upper-first-letter/upper-first-letter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UpperFirstLetterPipe } from './upper-first-letter.pipe';
-
-describe('UpperFirstLetterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UpperFirstLetterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
