9ff2fb58e30c78d5911e2d859870c0e48baed40e
RyanDrop <ryangithub7509@gmail.com>
2022-03-22 13:26:08 -0300

    ✨ feat(shared): firebase service

---
+++ src/app/shared/firebase/firebase.mock.ts
@@ -0,0 +1,8 @@
+import { RegisterUser } from 'app/shared/firebase';
+
+export const AOPA_TEST_USER: RegisterUser = {
+  name: 'Teste Teste',
+  email: 'teste@gmail.com',
+  password: '123456',
+  occupation: 'Quality Assurance',
+};
