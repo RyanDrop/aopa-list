64b6fcbdd199b2ee69e210fe566e6fa993580109
RyanDrop <ryangithub7509@gmail.com>
2022-03-26 23:55:00 -0300

    ✨ feat(project service): method add project

---
+++ src/app/shared/services/projects/projects.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Project } from 'app/shared/services/projects/projects.service.models';
+import { from, Observable, of } from 'rxjs';
+import { FirebaseService } from '../firebase/firebase.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+@UntilDestroy()
+export class ProjectsService {
+
+  private projects: Project[] = [];
+  project$: Observable<Project[]>
+
+  constructor(private firebase: FirebaseService) {
+    const aopaUser = from(this.firebase.getUser())
+    aopaUser.pipe(untilDestroyed(this)).subscribe(user => {
+      const projects = user.projects
+      this.project$ = of(projects)
+    })
+  }
+
+  addProject(project: Project) {
+    this.projects.push(
+      project
+    )
+    this.project$ = of(this.projects)
+    this.firebase.createProject(this.projects)
+  }
+}
