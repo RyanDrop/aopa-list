f70aadedb5e41c398447c0310f556d3113a7514b
RyanDrop <ryangithub7509@gmail.com>
2022-03-30 12:02:52 -0300

    ✨ feat(todo domain): project page

--- src/app/domain/todo/pages/project/project.page.ts
+++ src/app/domain/todo/pages/project/project.page.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { ProjectsService } from 'app/shared/services/projects/projects.service';
+import { Project } from 'app/shared/services/projects/projects.service.models';
+import { Observable } from 'rxjs';
 
-
+@UntilDestroy()
 @Component({
   selector: 'aopa-projects',
   templateUrl: './project.page.html',
   styleUrls: ['./project.page.scss']
 })
-export class ProjectPage {
+export class ProjectPage implements OnInit {
 
 
 
+  constructor(private route: ActivatedRoute, private projectsService: ProjectsService) { }
 
+  ngOnInit() {
+    this.route.params
+      .pipe(untilDestroyed(this))
+      .subscribe((params) => {
+        const id = params.id
+        this.projectsService.setProject(id)
+      });
+  }
+
+  get project$(): Observable<Project> {
+    return this.projectsService.project$
+  }
+
+  addTask(subProjectiD: number, task: string) {
+    this.projectsService.addTaskInSubProject(subProjectiD, task)
+  }
+
+  addSubProject(subProject: string) {
+    this.projectsService.addSubProject(subProject)
+  }
 }
--- src/app/domain/todo/todo.domain.module.ts
+++ src/app/domain/todo/todo.domain.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -48,6 +49,7 @@ import { TodoComponent } from './todo.component';
     HeaderWidgetModule,
     MatButtonModule,
     MatInputModule,
+    MatExpansionModule,
   ],
   exports: [],
 })
