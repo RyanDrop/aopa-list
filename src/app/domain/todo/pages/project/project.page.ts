81cd2a086785bd3a84b3621ca2abe445800fc06d
RyanDrop <ryangithub7509@gmail.com>
2022-03-31 21:58:59 -0300

    ✨ feat(page project in todo domain): firebase instance and set project with ng on init

--- src/app/domain/todo/pages/project/project.page.ts
+++ src/app/domain/todo/pages/project/project.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { FirebaseService } from 'app/shared/services/firebase/firebase.service';
 import { ProjectsService } from 'app/shared/services/projects/projects.service';
 import { Project } from 'app/shared/services/projects/projects.service.models';
-import { Observable } from 'rxjs';
+import { from, Observable, of, throwError } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @UntilDestroy()
 @Component({
@@ -13,21 +15,33 @@ import { Observable } from 'rxjs';
 })
 export class ProjectPage implements OnInit {
 
+  project$: Observable<Project>
 
-
-  constructor(private route: ActivatedRoute, private projectsService: ProjectsService) { }
+  constructor(private route: ActivatedRoute, public projectsService: ProjectsService, private firebase: FirebaseService) { }
 
   ngOnInit() {
-    this.route.params
-      .pipe(untilDestroyed(this))
-      .subscribe((params) => {
-        const id = params.id
-        this.projectsService.setProject(id)
-      });
-  }
+    const aopaUser = from(this.firebase.getUser())
+    aopaUser.pipe(untilDestroyed(this)).subscribe(aopa => {
+      if (aopa.user.darkThemePreference) {
+        document.documentElement.classList.add('dark-mode')
+      }
+    })
 
-  get project$(): Observable<Project> {
-    return this.projectsService.project$
+    this.route.params
+      .pipe(untilDestroyed(this), switchMap((params) => {
+        const projects = this.projectsService.getProjects()
+        return projects.pipe(
+          switchMap((projects) => {
+            const project = projects.find((project) => project.id == params.id)
+            return project ? of(project) : throwError('Project not found')
+          }
+          ))
+      })
+      ).subscribe((project) => {
+        this.projectsService.setProject(project)
+        this.project$ = of(project)
+        this.projectsService.getPercentageTasks()
+      })
   }
 
   addTask(subProjectiD: number, task: string) {
